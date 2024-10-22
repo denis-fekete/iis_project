ce072aef22f5856d34b0e4523d9108b3a58ce9b1
Nikita Kotvitskiy <nikita.kotvitski@gmail.com>
2024-10-22 23:22:41 +0200

    added database migrations

---
+++ database/migrations/2024_10_22_202801_create_conference_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conference', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('theme');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->string('place_address');
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('capacity');
+
+            $table->unsignedBigInteger('owner_id');
+            
+            $table->foreign('owner_id')
+                ->references('id')
+                ->on('user')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conference');
+    }
+};
---
+++ database/migrations/2024_10_22_204408_create_lecture_schedule_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lecture_schedule', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('lecture_id');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('user')
+                ->onDelete('cascade');
+            
+            $table->foreign('lecture_id')
+                ->references('id')
+                ->on('lecture')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lecture_schedule');
+    }
+};
